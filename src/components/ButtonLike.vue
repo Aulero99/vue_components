b027e7a0b54450655b37fed04e101c39a8f3fb6a
Auston Robertson <auston.robertson@gmail.com>
2023-07-20 11:38:29 -0600

    init

---
+++ src/components/ButtonLike.vue
@@ -0,0 +1,20 @@
+<template>
+  Content
+</template>
+  
+<script>
+  export default {
+    props:{
+
+    },
+    setup() {
+      return {
+  
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
